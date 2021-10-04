6e1547b90c2626ad47e8c3d30203ff671cc3f32c
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-10-04 19:19:37 +1100

    Update mongoose connect in seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect('https://rocky-sands-44055.herokuapp.com/', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
